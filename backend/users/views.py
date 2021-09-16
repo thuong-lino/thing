773590e2dba5cfdcae76e93d7c794861a948894f
thuong-lino <stevenguyen1701@gmail.com>
2021-09-16 22:10:42 +0700

    api log in

--- backend/users/views.py
+++ backend/users/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .serializers import UserSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import action
+from django.contrib.auth import login, authenticate
 from .models import User
 
 # Create your views here.
@@ -24,4 +25,19 @@ class UserViewSet(viewsets.ModelViewSet):
             if serializer.create(serializer.data):
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    @action(
+        detail=False,
+        methods=['post', 'get'],
+        permission_classes=[AllowAny],
+        url_path='login',
+    )
+    def login(self, request):
+        user = authenticate(
+            request, username=request.POST['email'], password=request.POST['password'])
+        print(user)
+        if user is not None:
+            login(request, user)
+            return Response({"message": "You are logged in"}, status=status.HTTP_200_OK)
         return Response(status=status.HTTP_400_BAD_REQUEST)
