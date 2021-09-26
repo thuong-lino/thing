eac259133c81ebb52c6b6ba68b6db771ca6ebb11
thuong-lino <stevenguyen1701@gmail.com>
2021-09-26 18:57:00 +0700

    a

--- backend/thing/settings/base.py
+++ backend/thing/settings/base.py
@@ -80,8 +80,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "common.context_processors.sentry_dsn",
-                "common.context_processors.commit_sha",
+                # "common.context_processors.sentry_dsn",
+                # "common.context_processors.commit_sha",
             ],
             "loaders": [
                 (
