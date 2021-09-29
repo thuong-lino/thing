08341194773e1231b42290a7662adb816b3c3dbd
thuong-lino <stevenguyen1701@gmail.com>
2021-09-29 10:41:49 +0700

    add session cookie age

--- backend/thing/settings/base.py
+++ backend/thing/settings/base.py
@@ -173,3 +173,5 @@ SESSION_COOKIE_SAMESITE = None
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+SESSION_COOKIE_AGE = 60
